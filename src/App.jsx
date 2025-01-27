2d035aae7ac0220dbe90ecd6226a99eef605c85b
Aaron Mendez <aarxnmendezz@gmail.com>
2025-01-27 19:54:35 +0100

    add local storage to persist game state between session

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,19 @@ import { Square } from "./components/Square.jsx"
 import { TURNS } from "./constants.js"
 import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
 import { WinnerModal } from "./components/WinnerModal.jsx"
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js"
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   // null there is no winner, false is a draw
   const [winner, setWinner] = useState(null)
@@ -18,6 +26,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const updateBoard = (index) => {
@@ -33,6 +43,12 @@ function App() {
     // Change turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // Save game
+    saveGameToStorage({ 
+      board: newBoard, 
+      turn: newTurn 
+    })
 
     // Check if there is a winner
     const newWinner = checkWinnerFrom(newBoard)
@@ -73,7 +89,7 @@ function App() {
         </Square>
       </section>
 
-      <WinnerModal resetGame={resetGame} winner={winner}/>
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   )
 }
